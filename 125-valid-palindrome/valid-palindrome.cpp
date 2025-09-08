d3ceb19e9c4088a9e02eafbda4b87eca2f7e362c
Vartika8052 <153089598+Vartika8052@users.noreply.github.com>
2025-09-08 22:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.07%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    bool valid(char ch) {
+        return (ch >= 'a' && ch <= 'z') || 
+               (ch >= 'A' && ch <= 'Z') || 
+               (ch >= '0' && ch <= '9');
+    }
+
+    char toLower(char ch) {
+        if (ch >= 'A' && ch <= 'Z')
+            return ch - 'A' + 'a';
+        return ch;
+    }
+
+    bool checkPalindrome(string a) {
+        int s = 0;
+        int e = a.length() - 1;
+
+        while (s <= e) {
+            if (a[s] != a[e]) {
+                return false;
+            }
+            s++;
+            e--;
+        }
+        return true;
+    }
+
+public:
+    bool isPalindrome(string s) {
+        string temp = "";   // FIX: empty string, not " "
+
+        for (int j = 0; j < s.length(); j++) {
+            if (valid(s[j])) {
+                temp.push_back(toLower(s[j])); // directly push lowercase
+            }
+        }
+
+        return checkPalindrome(temp);
+    }
+};
